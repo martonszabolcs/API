f28f80dfb57c398ad7f129b2c454ccc577c9c104
Marton Szabolcs <martar1994@gmail.com>
2018-05-16 20:22:36 +0200

    new

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -2,7 +2,7 @@ import { sign } from '../../services/jwt'
 import { success } from '../../services/response/'
 
 export const login = ({ user }, res, next) =>
-  sign(user.id)
+  sign(user.id) 
     .then((token) => ({ token, user: user.view(true) }))
     .then(success(res, 201))
     .catch(next)
